9cc262c7c0c04345cd1118cb188838f8d45d9787
Wendy Yansah <wendycoolzzz74@gmail.com>
2022-01-05 13:21:25 +0700

    Fixed some bugs on main script and test module.

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from unittest import main
 from arithmetic_arranger import arithmetic_arranger
 
 print(arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"]))
+
+main(module='test_module', exit=False)
